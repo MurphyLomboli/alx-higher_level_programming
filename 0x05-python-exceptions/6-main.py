d961aecd72a0bcb9083f1c84437ffee1efdc48e4
Murphy Lomboli <murphylomboli@outlook.com>
2023-01-23 05:11:31 -0800

    Tasks

---
+++ 0x05-python-exceptions/6-main.py
@@ -0,0 +1,9 @@
+table File  7 lines (6 sloc)  177 Bytes
+
+#!/usr/bin/python3
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
