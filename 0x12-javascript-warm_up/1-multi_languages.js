2bc69f89dfb06c6a07f023ada9ecce6d71a62f09
Murphy Lomboli <murphylomboli@outlook.com>
2023-04-20 09:33:25 -0700

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/**
+ * myVar -  represents the variables
+ * Autho - Murphy
+ */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("Javascript is amazing");
